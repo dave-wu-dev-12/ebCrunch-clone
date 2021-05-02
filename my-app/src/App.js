ed072f4efda0549afb8dde52b1a400fcc313e348
dave-wu-dev-12 <davidwwu12@gmail.com>
2021-05-02 12:38:38 -0500

    adding initial layer for dark mode toggle

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import styled, {
+  css,
+  ThemeProvider,
+  createGlobalStyle,
+} from "styled-components";
+import { useState } from "react";
+
+import storage from "local-storage-fallback";
+
+// initial theme object for our theme state
+const initialTheme = {
+  mode: "dark",
+  darkModeColor: "black",
+  lightModeColor: "wheat",
+};
+
+// global style values like the body etc
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: ${(props) =>
+      props.theme.mode == "dark"
+        ? props.theme.darkModeColor
+        : props.theme.lightModeColor};
+    color:${(props) =>
+      props.theme.mode == "dark"
+        ? props.theme.lightModeColor
+        : props.theme.darkModeColor};
+  }
+`;
 
 function App() {
+  // this can go either outside or inside the funciton app
+  // it just has to be before the useState
+  const getInitialTheme = () => {
+    let storageTheme = storage.getItem("appTheme");
+    return storageTheme ? JSON.parse(storageTheme) : initialTheme;
+  };
+
+  // usestate will call the get initial func once during the intial load render
+  // so we can skip the use effect here in lieu of this
+  const [theme, setTheme] = useState(getInitialTheme);
+
+  const setAppTheme = () => {
+    let chosenTheme =
+      theme.mode == "dark"
+        ? { ...theme, mode: "light" }
+        : { ...theme, mode: "dark" };
+
+    storage.setItem("appTheme", JSON.stringify(chosenTheme));
+
+    setTheme(chosenTheme);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <div className="App">
+        <div>Energy bill cruncher</div>
+        <button onClick={() => setAppTheme()}>Toggle Mode</button>
+      </div>
+    </ThemeProvider>
   );
 }
 
